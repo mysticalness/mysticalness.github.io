9d137e57b569882673c9d7e54845544845e5baf5
mysticalness <kih9079@naver.com>
2023-12-30 23:57:08 +0900

    hello

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const photoClick = document.querySelector("#profilePhoto");
+  photoClick.onclick = function() {
+    location.href = '/pages/main.html';
+  }
+ });
+
+
